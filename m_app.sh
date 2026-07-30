76c79e080a25387c23bb2dcdf5c657c07d7d2b23
quartersawn <n5tin.tx@gmail.com>
2026-07-30 20:33:16 +0000

    Add copyright for Steve - N5TIN for 2026 in m_app.sh

--- m_app.sh
+++ m_app.sh
@@ -6,6 +6,7 @@
 # Copyright (C) 2024 Freddie Mac - KD5FMU 
 # Copyright (C) 2024 Allan - OCW3AW
 # Copyright (C) 2024-2025 Jory A. Pratt - W5GLE
+# Copyright (C) 2026 Steve - N5TIN
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
